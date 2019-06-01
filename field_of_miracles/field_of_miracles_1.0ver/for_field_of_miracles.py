48e2df227af71f6050e84768f5e09117bfe55333
hackerrank.com <44140614+dmitrii1991@users.noreply.github.com>
2019-06-01 16:14:47 +0300

    Create for_field_of_miracles.py

---
+++ field_of_miracles/field_of_miracles_1.0ver/for_field_of_miracles.py
@@ -0,0 +1,7 @@
+WORD = [
+    {'number': '1', 'key': 'conflict', 'def': 'an active disagreement between people with opposing opinions or principles:'},
+    {'number': '2', 'key': 'passion', 'def': 'a very powerful feeling, for example of sexual attraction, love, hate, anger, or other emotion'}
+]
+PLAYERS = ['Sam', 'Vania', 'Alsu', 'Eric']
+greetings = 'WELCOME IN THE GAME "FIELD OF MIRACLES"'
+LETTERS = 'qwertyuiopasdfghjklzxcvbnm'
