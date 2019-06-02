daa0d809f249251e132db511081cd7e5c1f35338
hackerrank.com <44140614+dmitrii1991@users.noreply.github.com>
2019-06-02 13:43:15 +0300

    Create main.py

---
+++ flask/first_work/main.py
@@ -0,0 +1,20 @@
+from find_in_phrase import find
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/find', methods=['POST'])
+def to_find() -> 'html':
+    phrase = request.form['word/phrase']
+    letters = request.form['letters']
+    title = 'Here are your results:'
+    results = str(find(phrase, letters))
+    return render_template('results.html', the_phrase=phrase, the_letters=letters, the_title=title, the_results=results,)
+
+@app.route('/')
+@app.route('/entry')
+def entry_psge() -> 'html':
+    return render_template("entry.html", the_title='Welkome to the prjgramm!!')
+
+app.run(debug=True)
